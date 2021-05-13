2234a494ac7d40d757596c64e40a58dd993fc145
aidanblack <aidanblack@me.com>
2021-05-13 14:03:43 -0500

    Enhance watch face and AOD

--- app/index.js
+++ app/index.js
@@ -6,7 +6,8 @@ import { BodyPresenceSensor } from "body-presence";
 import { today } from "user-activity";
 import { goals } from "user-activity";
 import { battery } from "power";
-import * as weather from 'fitbit-weather/app';
+import { display } from "display";
+import * as weather from "fitbit-weather/app";
 
 clock.granularity = "seconds";
 
@@ -15,15 +16,20 @@ const dateBox = document.getElementById("dateBox")
 const hourHand = document.getElementById("hourHand");
 const minuteHand = document.getElementById("minuteHand");
 const secondHand = document.getElementById("secondHand");
+const hourHandShadow = document.getElementById("hourHandShadow");
+const minuteHandShadow = document.getElementById("minuteHandShadow");
+const secondHandShadow = document.getElementById("secondHandShadow");
 const statsBox = document.getElementById("statsBox");
 const statsArc = document.getElementById("statsArc");
 const weatherArc = document.getElementById("weatherArc");
 const statsImage = document.getElementById("statsImage");
+const marks = document.getElementsByClassName("marks");
+
 const hrm;
 var heartRate = 0;
 if (HeartRateSensor) {
-        hrm = new HeartRateSensor({ frequency: 1 });
-        hrm.addEventListener("reading", () => {
+    hrm = new HeartRateSensor({ frequency: 1 });
+    hrm.addEventListener("reading", () => {
         heartRate = hrm.heartRate;
     });
     hrm.start();
@@ -32,6 +38,7 @@ var weatherResult;
 var statDisplay = 0;
 var currentCount = 0;
 var currentGoal = 1;
+var darkMode = false;
 
 const modes = {
     Battery: "Battery",
@@ -45,33 +52,74 @@ const modes = {
 };
 var mode = modes.Weather;
 
-clockFace.addEventListener("click", (evt) => {
-    let marks = document.getElementsByClassName("marks");
-    if(clockFace.style.fill == "#FFFFF9") {
+function switchDarkMode() {
+    if (darkMode || (display.aodActive && display.off)) {
         clockFace.style.fill = "#111111";
+        statsBox.style.fill = "white";
+        statsBox.style.opacity = 0.1;
         marks.forEach((element) => {
             element.style.fill = "#FFFFF9"
         });
     }
-    else {
+    else if(!darkMode && !display.aodActive && display.on) {
         clockFace.style.fill = "#FFFFF9";
+        statsBox.style.fill = "#999999";
+        statsBox.style.opacity = 0.05;
         marks.forEach((element) => {
             element.style.fill = "black"
         });
     }
+}
+
+clockFace.addEventListener("click", (evt) => {
+    if(darkMode) darkMode = false;
+    else darkMode = true;
+    switchDarkMode();
 });
+
+if (display.aodAvailable && me.permissions.granted("access_aod")) {
+    // tell the system we support AOD
+    display.aodAllowed = true;
+
+    // respond to display change events
+    display.addEventListener("change", () => {
+        switchDarkMode();
+        // Is AOD inactive and the display is on?
+        if (!display.aodActive && display.on) {
+            hrm.start();
+            clock.granularity = "seconds";
+
+            let aod = document.getElementsByClassName("aod");
+            aod.forEach((element) => {
+                element.style.visibility = "visible";
+            });
+        }
+        else {
+            hrm.stop();
+            clock.granularity = "minutes";
+
+            let aod = document.getElementsByClassName("aod");
+            aod.forEach((element) => {
+                element.style.visibility = "hidden";
+            });
+        }
+    });
+}
 
 clock.ontick = (evt) => {
     let now = evt.date;
     let dateText = now.toLocaleString('default', { month: 'short' }).substring(4, 10);
-  
+
     dateBox.text = dateText.toUpperCase();
     let hours = now.getHours();
     hours = hours % 12 || 12;
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
+    hourHandShadow.groupTransform.rotate.angle = ((360 / 12) * hours) + ((360 / 12 / 60) * minutes);
     hourHand.groupTransform.rotate.angle = ((360 / 12) * hours) + ((360 / 12 / 60) * minutes);
+    minuteHandShadow.groupTransform.rotate.angle = (360 / 60) * minutes + ((360 / 60 / 60) * seconds);
     minuteHand.groupTransform.rotate.angle = (360 / 60) * minutes + ((360 / 60 / 60) * seconds);
+    secondHandShadow.groupTransform.rotate.angle = seconds * 6;
     secondHand.groupTransform.rotate.angle = seconds * 6;
 
     if (me.permissions.granted("access_activity")) updateStats(mode);
@@ -81,48 +129,48 @@ statsBox.addEventListener("click", (evt) => { changeMode(); });
 statsImage.addEventListener("click", (evt) => { changeMode(); });
 
 function changeMode() {
-    if(mode == modes.Battery) {
+    if (mode == modes.Battery) {
         mode = modes.Weather;
         statsArc.style.visibility = "hidden";
         weatherArc.style.visibility = "visible";
         statsImage.href = "";
     }
-    else if(mode == modes.Weather) {
+    else if (mode == modes.Weather) {
         mode = modes.HeartRate;
         statsArc.style.fill = "#CC2222";
         statsImage.href = "heart_rate_36px.png";
     }
-    else if(mode == modes.HeartRate){
+    else if (mode == modes.HeartRate) {
         mode = modes.Steps;
         statsArc.style.fill = "#EFC12B"; // yellow
         statsImage.href = "steps_36px.png";
     }
-    else if(mode == modes.Steps) {
+    else if (mode == modes.Steps) {
         mode = modes.Distance;
         statsArc.style.fill = "#006EEF"; // blue
         statsImage.href = "distance_36px.png";
     }
-    else if(mode == modes.Distance) {
+    else if (mode == modes.Distance) {
         mode = modes.Floors;
         statsArc.style.fill = "#EF6EEF"; // purple
         statsImage.href = "floors_36px.png";
     }
-    else if(mode == modes.Floors) {
+    else if (mode == modes.Floors) {
         mode = modes.Calories;
         statsArc.style.fill = "#00ccc2"; // teal
         statsImage.href = "calories_36px.png";
     }
-    else if(mode == modes.Calories) {
+    else if (mode == modes.Calories) {
         mode = modes.Zone;
         statsArc.style.fill = "#EA7D1E"; // orange
         statsImage.href = "azm_36px.png";
     }
-    else if(mode == modes.Zone) {
+    else if (mode == modes.Zone) {
         mode = modes.Battery;
         statsArc.style.fill = "#3FD300"; //green
         statsImage.href = "battery_36px.png";
     }
-    if(mode != modes.Weather) {
+    if (mode != modes.Weather) {
         statsArc.style.visibility = "visible";
         weatherArc.style.visibility = "hidden";
     }
@@ -141,56 +189,56 @@ function processWeather(weather) {
 
     statDisplay = currentCount / currentGoal * 360;
     statDisplay = Math.round(statDisplay);
-    if(statDisplay > 360) statDisplay = 360;
+    if (statDisplay > 360) statDisplay = 360;
     weatherArc.sweepAngle = statDisplay;
 }
 
 function updateStats(mode) {
-    if(mode == modes.Battery) {
+    if (mode == modes.Battery) {
         currentCount = battery.chargeLevel;
         currentGoal = 100;
     }
 
-    if(mode == modes.Weather) {
+    if (mode == modes.Weather) {
         weather.fetch(30 * 60 * 1000) // return the cached value if it is less than 30 minutes old 
-        .then(weather => processWeather(weather))
-        .catch(error => console.log(JSON.stringify(error)));
+            .then(weather => processWeather(weather))
+            .catch(error => console.log(JSON.stringify(error)));
     }
 
-    if(mode == modes.HeartRate) {
+    if (mode == modes.HeartRate) {
         currentCount = heartRate;
         currentGoal = 200;
     }
 
-    if(mode == modes.Steps) {
+    if (mode == modes.Steps) {
         currentCount = today.adjusted.steps;
         currentGoal = goals.steps;
     }
-    
-    if(mode == modes.Distance) {
+
+    if (mode == modes.Distance) {
         currentCount = today.adjusted.distance;
         currentGoal = goals.distance;
     }
 
-    if(mode == modes.Floors) {
+    if (mode == modes.Floors) {
         currentCount = today.adjusted.elevationGain;
         currentGoal = goals.elevationGain;
     }
 
-    if(mode == modes.Calories) {
+    if (mode == modes.Calories) {
         currentCount = today.adjusted.calories;
         currentGoal = goals.calories;
     }
 
-    if(mode == modes.Zone) {
+    if (mode == modes.Zone) {
         currentCount = today.adjusted.activeZoneMinutes.total;
         currentGoal = goals.activeZoneMinutes.total;
     }
 
-    if(mode != modes.Weather) {
+    if (mode != modes.Weather) {
         statDisplay = currentCount / currentGoal * 300;
         statDisplay = Math.round(statDisplay);
-        if(statDisplay > 300) statDisplay = 300;
+        if (statDisplay > 300) statDisplay = 300;
         statsArc.sweepAngle = statDisplay;
     }
 }
--- companion/index.js
+++ companion/index.js
@@ -4,4 +4,4 @@ import { me as companion } from "companion";
 import * as weather from 'fitbit-weather/companion';
 
 /* Api Key can be obtained from openweathermap.com */
-weather.setup({ provider: weather.Providers.openweathermap, apiKey: '2b98f571a72f1717c4bb4a6ccdba4533' });
+weather.setup({ provider: weather.Providers.openweathermap, apiKey: '<API Key>' });
